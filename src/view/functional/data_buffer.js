40423a2b3783c84236e9e181785bc930ce35fe3a
illuminoplanet <illuminoplanet@gmail.com>
2022-08-30 15:56:43 +0900

    FEAT: Add data buffer class in view

---
+++ src/view/functional/data_buffer.js
@@ -0,0 +1,14 @@
+export class DataBuffer {
+    constructor() {
+        this.buffer = {}
+    }
+    append(json) {
+        const key = Object.keys(json)[0]
+        const data = json[key]
+
+        if (!(key in this.buffer)) {
+            this.buffer[key] = []
+        }
+        this.buffer[key].append(data)
+    }
+}
