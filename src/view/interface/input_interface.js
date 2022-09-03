2fa2cb95cc0d4f23e53ed7ecd2491088a69a537a
김주영 <illuminoplanet@gmail.com>
2022-09-03 13:47:44 +0000

    FEAT: Add buttons in input interface class

--- src/view/interface/input_interface.js
+++ src/view/interface/input_interface.js
@@ -1,9 +1,19 @@
 import { Button } from "./component/button.js"
 
-export class InputComponent {
+export class InputInterface {
     constructor(event_handler) {
+        this.nav = document.createElement("nav")
+        document.body.appendChild(this.nav)
+        
         this.components = [
-            new Button("reshape_environment", event_handler)
+            new Button("reshape_environment", event_handler), 
+            new Button("change_algorithm", event_handler), 
+            new Button("toggle_run", event_handler), 
+            new Button("reset_environment", event_handler), 
+            new Button("toggle_visualization", event_handler), 
         ]
+        for (let button in this.components) {
+            this.nav.appendChild(button)
+        }
     }
 }
