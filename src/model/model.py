ff725a48903fa2240ee29abed15d6ca94671164f
김주영 <illuminoplanet@gmail.com>
2022-09-03 03:18:07 +0000

    FEAT: Add reshape environment method in model class

--- src/model/model.py
+++ src/model/model.py
@@ -1,3 +1,12 @@
+from env import GridWorldEnv
+
+
 class Model:
     def __init__(self):
-        pass
+        self.env = GridWorldEnv(2, 2, 2)
+    
+    def reshape_environment(self, shape):
+        del self.env 
+        self.env = GridWorldEnv(shape)
+        
+        return self.env.get_information()
