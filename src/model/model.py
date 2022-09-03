3507344d126eb2cca272594fc7cd8d13779da883
김주영 <illuminoplanet@gmail.com>
2022-09-03 11:23:51 +0000

    FEAT: Add change algorithm method in model class

--- src/model/model.py
+++ src/model/model.py
@@ -1,12 +1,20 @@
 from env import GridWorldEnv
+from agent import Agent
 
 
 class Model:
     def __init__(self):
         self.env = GridWorldEnv(2, 2, 2)
+        self.agent = Agent(self.env.get_property())
     
     def reshape_environment(self, shape):
         del self.env 
         self.env = GridWorldEnv(shape)
+        
+        return self.env.get_information()
+    
+    def change_algorithm(self, algorithm):
+        self.env.reset()
+        self.agent.change_algorithm(algorithm)
         
         return self.env.get_information()
