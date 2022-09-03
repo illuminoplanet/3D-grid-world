fc8c841d56dcf10bc8d21ab77e077fdec16d323f
김주영 <illuminoplanet@gmail.com>
2022-09-03 11:22:34 +0000

    FEAT: Add agent class in model

---
+++ src/model/agent/agent.py
@@ -0,0 +1,18 @@
+from algorithm.policy_iteration import PolicyIteration
+
+
+class Agent:
+    def __init__(self, env_property):
+        self.obs_space = env_property["obs_space"]
+        self.action_space = env_property["action_space"]
+        self.model = env_property["model"]
+        
+        self.algorithm_map = {
+            "policy_iteration" : PolicyIteration
+        }
+        self.algorithm = PolicyIteration(self.obs_space, self.action_space, self.model)
+        
+    def change_algorithm(self, algorithm):
+        del self.algorithm
+        self.algorithm = self.algorithm_map[algorithm](self.obs_space, self.action_space, self.model)
+        
