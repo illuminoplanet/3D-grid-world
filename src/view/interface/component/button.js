b6de9dc54e59785556fc9c6b1d3f9c1298868306
illuminoplanet <illuminoplanet@gmail.com>
2022-09-14 16:23:29 +0900

    FIX: Update input interface to connect event handler to button

--- src/view/interface/component/button.js
+++ src/view/interface/component/button.js
@@ -1,9 +1,9 @@
 export class Button {
-	constructor(type) {
+	constructor(type, event_handler) {
 		this.button = document.createElement("button")
 		this.button.className = "button"
 		this.button.innerText = type
-		// this.button.onclick = () => event_handler.handle(type)
+		this.button.onclick = () => event_handler.handle(type)
 		
 		document.body.appendChild(this.button)
 		return this.button
--- src/view/interface/input_interface.js
+++ src/view/interface/input_interface.js
@@ -3,14 +3,14 @@ import { Button } from "./component/button.js"
 
 
 export class InputInterface {
-    constructor() {
+    constructor(event_handler) {
         this.components = [
             new Sidebar([
-                new Button("reshape_environment",this.event_queue), 
-                new Button("change_algorithm", this.event_queue), 
-                new Button("toggle_run", this.event_queue), 
-                new Button("reset_environment", this.event_queue), 
-                new Button("toggle_visualization", this.event_queue)
+                new Button("reshape_environment", event_handler), 
+                new Button("change_algorithm", event_handler), 
+                new Button("toggle_run", event_handler), 
+                new Button("reset_environment", event_handler), 
+                new Button("toggle_visualization", event_handler)
             ])
         ]
     }
